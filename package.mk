213ddac3dc41cdf23694973b1819ae6e6431a953
David Wragg <david@rabbitmq.com>
2010-12-02 12:32:22 +0000

    Overhaul for bug23568

--- package.mk
+++ package.mk
@@ -10,27 +10,26 @@ INCLUDE_DIR:=$(CHECKOUT_DIR)/include
 
 ERLC_OPTS:=-I $(CHECKOUT_DIR)
 
-$(CHECKOUT_DIR)_UPSTREAM_HG:=$(UPSTREAM_HG)
-$(CHECKOUT_DIR)_REVISION:=$(REVISION)
-$(CHECKOUT_DIR):
-	hg clone -r $($@_REVISION) $($@_UPSTREAM_HG) $@
-
-$(CHECKOUT_DIR)/stamp: | $(CHECKOUT_DIR)
-	rm -f $@
-	cd $(@D) && echo COMMIT_SHORT_HASH:=$$(hg id -i | cut -c -7) > $@
-
-$(PACKAGE_DIR)/clean_RM:=$(CHECKOUT_DIR) $(CHECKOUT_DIR)/stamp $(EBIN_DIR)/$(APP_NAME).app
-$(PACKAGE_DIR)/clean::
-	rm -rf $($@_RM)
-
-ifneq "$(strip $(patsubst clean%,,$(patsubst %clean,,$(TESTABLEGOALS))))" ""
-include $(CHECKOUT_DIR)/stamp
+$(eval $(call safe_include,$(PACKAGE_DIR)/version.mk))
 
 VERSION:=rmq$(GLOBAL_VERSION)-hg$(COMMIT_SHORT_HASH)
 
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION)_VERSION:=$(VERSION)
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION): $(CHECKOUT_DIR)/ebin/$(APP_NAME).app | $(EBIN_DIR)
-	sed -e 's/{vsn, *\"[^\"]\+\"/{vsn,\"$($@_VERSION)\"/' < $< > $@
+define package_targets
 
-$(PACKAGE_DIR)_APP:=true
-endif
+$(CHECKOUT_DIR)/.done:
+	rm -rf $(CHECKOUT_DIR)
+	hg clone -r $(REVISION) $(UPSTREAM_HG) $(CHECKOUT_DIR)
+	patch -d $(CHECKOUT_DIR) -p1 <$(PACKAGE_DIR)/uneunit.patch
+	touch $$@
+
+$(PACKAGE_DIR)/version.mk: $(CHECKOUT_DIR)/.done
+	echo COMMIT_SHORT_HASH:=`hg id -R $(CHECKOUT_DIR) -i | cut -c -7` >$$@
+
+$(EBIN_DIR)/$(APP_NAME).app: $(CHECKOUT_DIR)/ebin/$(APP_NAME).app $(PACKAGE_DIR)/version.mk
+	@mkdir -p $$(@D)
+	sed -e 's|{vsn, *\"[^\"]*\"|{vsn,\"$(VERSION)\"|' <$$< >$$@
+
+$(PACKAGE_DIR)+clean::
+	rm -rf $(CHECKOUT_DIR) $(EBIN_DIR)/$(APP_NAME).app $(PACKAGE_DIR)/version.mk
+
+endef
