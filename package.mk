e62156549717405457c71cd986df8fac78e6f499
David Wragg <david@rabbitmq.com>
2011-02-03 11:54:17 +0000

    Unify wrapper and non-wrapper package app file generation

--- package.mk
+++ package.mk
@@ -5,6 +5,6 @@ UPSTREAM_HG:=http://bitbucket.org/justin/webmachine
 UPSTREAM_REVISION:=0c4b60ac68b4
 WRAPPER_PATCHES:=uneunit.patch 10-crypto.patch
 
-UPSTREAM_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
+ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 
 ERLC_OPTS:=-I $(CLONE_DIR)
