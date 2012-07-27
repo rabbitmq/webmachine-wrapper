a0abed2219e794d1dbab2c5d9efe56b0774924b8
Tim Watson <tim@rabbitmq.com>
2012-07-27 12:10:18 -0400

    Backport 4a52826bbab0 (Merge of bug25050; Bug 24975 broke rabbitmq-jsonrpc-channel)

--- package.mk
+++ package.mk
@@ -1,17 +1,14 @@
 APP_NAME:=webmachine
 DEPS:=mochiweb-wrapper
 
-UPSTREAM_HG:=http://bitbucket.org/justin/webmachine
-UPSTREAM_REVISION:=0c4b60ac68b4
+UPSTREAM_GIT:=https://github.com/basho/webmachine.git
+UPSTREAM_REVISION:=52e62bc655d700dfc8f409d44db0a07b39c3234f
 RETAIN_ORIGINAL_VERSION:=true
 
-WRAPPER_PATCHES:=uneunit.patch 10-crypto.patch xref-fix.patch ssl.patch
+WRAPPER_PATCHES:=10-remove-crypto-dependency.patch
 
 ORIGINAL_APP_FILE=$(CLONE_DIR)/ebin/$(APP_NAME).app
 DO_NOT_GENERATE_APP_FILE=true
-
-# Webmachine source files do -include("include/...")
-PACKAGE_ERLC_OPTS+=-I $(CLONE_DIR)
 
 define package_rules
 
